a3ebffe2b0a078602180d45c7870404d2b2c6b34
JezzRodriguez <jrodriguez@appdev.yearup.org>
2023-05-26 11:58:40 -0400

    adding comments to function for understanding

--- mountains/mountain.js
+++ mountains/mountain.js
@@ -1,29 +1,48 @@
 
+//runs the main function when the page is loaded
 window.onload = main;
 
+// runs the function that populates dropdown when the page is loaded
 function main() {
     loadMountainsInfo();
 }
 
 function loadMountainsInfo() {
-    const selectElement = document.querySelector("#mountain");
+    // creates a variable that accesses the select from HTML
+    const selectElement = document.querySelector("#mountainselect");
+
+    // runs the function when the element on the select menu is changed
     selectElement.onchange = mountainChange;
 
-    for (const mountainData of mountainsArray) {
+    //loops through the array of mountains
+    for (const mountainData of mountainsArray){
+
+        //creates empty element
         const option = document.createElement("option");
-        option.value = mountainData.name;
-        option.innerText = mountainData.name;
-        selectElement.append(option);
-    }
+
+    //adds to the new element
+    option.value = mountainData.name;
+    option.innerText = mountainData.name;
+
+    //adds information the the branch that is already on the page
+    selectElement.append(option);
 }
-
+}
+//function that is ran on change event 
 function mountainChange() {
-    const selectElement = document.querySelector("#mountain");
+    const selectElement = document.querySelector("#mountainselect");
     const mountainInfo = document.querySelector("main");
-    const currentMountain = selectElement.value;
 
+    //creates variable in order to compare values of selected mountain
+    const selectedMountain = selectElement.value;
+
+    //loops through the array of mountains
     for (const mountain of mountainsArray) {
-        if (currentMountain === mountain.name) {
+
+        //if the mountain name in the iteriation of the array matches the mountain the user selcted 
+        if (selectedMountain === mountain.name) {
+
+            //then it will display array information using the variable previosly declared to display on HTML page
             mountainInfo.innerHTML = `
                 <div class="card transparent-card" >
                     <img src="../images/${mountain.img}" class="card-img-top" alt="Mountain Image">
